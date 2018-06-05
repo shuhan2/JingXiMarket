a77e9dc6994babecde5150ade1f6ac22f92847bd
shuhan <2472520650@qq.com>
2018-06-05 17:01:58 +0800

    init

---
+++ src/main/java/com/example/JingXiMarket/JingXiMarketApplication.java
@@ -0,0 +1,12 @@
+package com.example.JingXiMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingXiMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingXiMarketApplication.class, args);
+	}
+}
