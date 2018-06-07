c42d592bf49c9c158a3e961c78d97bff591351ae
shuhan <2472520650@qq.com>
2018-06-07 21:34:47 +0800

    enum

--- src/main/java/com/example/JingXiMarket/Enum/expressEnum.java
+++ src/main/java/com/example/JingXiMarket/Enum/expressEnum.java
@@ -1,4 +1,31 @@
 package com.example.JingXiMarket.Enum;
 
-public class expressEnum {
+public enum expressEnum {
+    CREATE(1,"create"),
+    TRANSPORT(2,"transport"),
+    SIGN(3,"sign"),
+    INVALID(5,"invalid");
+    private int expressStatusId;
+    private String expressStatus;
+    public int getExpressStatusId() {
+        return expressStatusId;
+    }
+
+    public void setExpressStatusId(int expressStatusId) {
+        this.expressStatusId = expressStatusId;
+    }
+
+    public String getExpressStatus() {
+        return expressStatus;
+    }
+
+    public void setExpressStatus(String expressStatus) {
+        this.expressStatus = expressStatus;
+    }
+
+
+    expressEnum(int expressStatusId,String expressStatus){
+        this.expressStatusId = expressStatusId;
+        this.expressStatus = expressStatus;
+    }
 }
--- src/main/java/com/example/JingXiMarket/Enum/orderEnum.java
+++ src/main/java/com/example/JingXiMarket/Enum/orderEnum.java
@@ -1,4 +1,44 @@
 package com.example.JingXiMarket.Enum;
 
-public class orderEnum {
+import com.example.JingXiMarket.entity.Order;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum orderEnum {
+    CTRATE(1,"create"),
+    PAID(2,"paid"),
+    UNDO(3,"undo"),
+    FINISH(4,"finish"),
+    INVALID(5,"invalid");
+    public static final Map<String, orderEnum> map = new HashMap<>();
+    public static  orderEnum mapping(String order){
+        if (map.containsKey(order)){
+            return map.get(order);
+        }
+        return orderEnum.INVALID;
+    }
+    private int orderStatusId;
+    private String orderStatus;
+    public int getOrderStatusId() {
+        return orderStatusId;
+    }
+
+    public void setOrderStatusId(int orderStatusId) {
+        this.orderStatusId = orderStatusId;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+
+    orderEnum(int orderStatusId,String orderStatus){
+        this.orderStatusId = orderStatusId;
+        this.orderStatus = orderStatus;
+    }
 }
